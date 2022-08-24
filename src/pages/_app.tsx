5e460d54fdca5745bed5233dc63f72f7408e0379
furuta03 <wewen915@gmail.com>
2022-08-24 10:21:01 +0900

    style(css): css to scss
    Because this project not use css.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '@/styles/globals.css';
+import '@/styles/globals.scss';
 import { StyledEngineProvider, ThemeProvider } from '@mui/material/styles';
 import type { AppProps } from 'next/app';
 import React from 'react';
